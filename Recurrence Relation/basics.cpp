cfb5abff87b7bcf093a240aa2de6633f19306912
Harsh Gupta <you@example.com>
2022-04-03 10:18:29 -0700

    Recurrence Relations Basics

---
+++ Recurrence Relation/basics.cpp
@@ -0,0 +1,19 @@
+/*
+
+Recurrence relation is that relation that reoccurs or calls itself again.
+
+T(n) = time taken to process the value of n.
+
+for Binary Search ->>>>  T(n) = T(n/2) + c    where c = some constant
+for some functions ->>>> T(n) = T(n-1) + c
+
+if n is not defined then T(n) = 1 (always).
+
+3 methods to find the time complexity using recurrence realtions:
+    1 . Substitution Method
+    2 . Master Theorum
+    3 . Recurrence Tree
+
+
+
+*/
